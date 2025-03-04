d29e6fd2cf1f6dfb851b39fa70dc9d0c7987d521
ThienDo99 <thien.do@watasoftware.com>
2025-03-04 09:35:17 +0700

    feat: init

---
+++ types/response.ts
@@ -0,0 +1,20 @@
+import { TooltipProps } from "@mui/material";
+
+interface IItem {
+  id: string;
+  url: string;
+  altText: string;
+  products: {
+    id: string;
+    dotCoordinates: {
+      x: number;
+      y: number;
+    };
+    tagPosition: TooltipProps["placement"];
+  }[];
+}
+
+export interface IData {
+  video_link: string;
+  items: IItem[];
+}
---
+++ utils/constants.ts
@@ -0,0 +1,1 @@
+export const FIBONACCI_INDECES = [1, 2, 3, 5, 8, 13, 21];
